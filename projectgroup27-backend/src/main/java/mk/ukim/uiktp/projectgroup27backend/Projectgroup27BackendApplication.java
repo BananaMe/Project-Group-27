021a0cdeab155e709536013aa28ec0c7334118c9
Miroslav Radikj <Miroslav.Radikj@netcetera.com>
2022-04-07 18:06:03 +0200

    setting up backend on remote repo

---
+++ projectgroup27-backend/src/main/java/mk/ukim/uiktp/projectgroup27backend/Projectgroup27BackendApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.uiktp.projectgroup27backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectgroup27BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projectgroup27BackendApplication.class, args);
+    }
+
+}
---
+++ projectgroup27-backend/src/test/java/mk/ukim/uiktp/projectgroup27backend/Projectgroup27BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.uiktp.projectgroup27backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectgroup27BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
